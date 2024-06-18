f7d739ff05b3ad17f310f6cc8686f697ac0cebe6
elpekenin <elpekenin@elpekenin.dev>
2024-06-18 21:56:28 +0200

    move submodules to `HAL::build.zig`

--- build.zig
+++ build.zig
@@ -23,18 +23,16 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     }).artifact("hal");
 
-    const rtt = b.dependency("rtt", .{
-        .target = target,
-        .optimize = optimize,
-    }).module("rtt");
+    const rtt = b.dependency("rtt", .{}).module("rtt");
 
-    const zfat_dependency = b.dependency("zfat", .{
+    const zfat = b.dependency("zfat", .{
         .target = target,
         .optimize = optimize,
         .@"static-rtc" = @as([]const u8, "2024-06-17"),
-    });
-    const zfat_artifact = zfat_dependency.artifact("zfat");
-    const zfat_module = zfat_dependency.module("zfat");
+        .@"no-libc" = true,
+        .long_file_name = false, // saves ~2.5KB in .ReleaseSmall
+        .tiny = true, // saves 200B in .ReleaseSmall
+    }).module("zfat");
 
     // *** Actual zig code ***
     const zig = b.addExecutable(.{
@@ -50,7 +48,7 @@ pub fn build(b: *std.Build) !void {
     inline for (.{
         hal,
         zig,
-        zfat_artifact,
+        zfat,
     }) |module| {
         module.linkLibrary(libc); // also pulls headers
     }
@@ -61,17 +59,8 @@ pub fn build(b: *std.Build) !void {
         .flags = &.{"-fno-sanitize=undefined"},
         .root = b.path("src/c"),
     });
-    inline for (.{
-        "src/c", // hal_conf.h
-        "lib/cmsis/Include", // TODO: Move to repo's build.zig
-        "lib/CMSIS_5/CMSIS/Core/Include", // TODO: Move to repo's build.zig
-    }) |path| {
-        // required for HAL to buiild
-        hal.addIncludePath(b.path(path));
+    hal.addIncludePath(b.path("src/c")); // hal_conf.h
 
-        // make them visible to anything linking against HAL (aka custom zig code)
-        hal.installHeadersDirectory(b.path(path), "", .{});
-    }
 
     // macros required on the toolchain level for HAL compilation
     // zig also requires them for `@cImport`, but are added with `@cDefine`
@@ -91,22 +80,36 @@ pub fn build(b: *std.Build) !void {
         hal.root_module.c_macros.append(b.allocator, macro) catch @panic("OOM");
     }
 
+    // strip unused symbols to save space, flash is small
+    inline for (.{
+        libc,
+        hal,
+    }) |module| {
+        module.link_gc_sections = true;
+        module.link_data_sections = true;
+        module.link_function_sections = true;
+    }
+
     zig.linkLibrary(hal);
+    zig.addIncludePath(b.path("src/c")); // hal_conf.h, for @cImport
 
     zig.root_module.addImport("rtt", rtt);
-    zig.root_module.addImport("fatfs", zfat_module);
+    zig.root_module.addImport("fatfs", zfat);
 
     zig.setLinkerScript(b.path("ld/bootloader.ld"));
 
     // *** .elf -> .bin ***
-    const elf = b.addInstallArtifact(zig, .{});
-    b.default_step.dependOn(&elf.step);
+    // b.default_step.dependOn(
+    //     &b.addInstallBinFile(
+    //         b.addObjCopy(
+    //             zig.getEmittedBin(),
+    //             .{.format = .bin}
+    //         ).getOutput(),
+    //         "app.bin"
+    //     ).step
+    // );
 
-    const elf2bin = b.addObjCopy(zig.getEmittedBin(), .{ .format = .bin });
-    elf2bin.step.dependOn(&elf.step);
-
-    const bin = b.addInstallBinFile(elf2bin.getOutput(), "app.bin");
-    b.default_step.dependOn(&bin.step);
+    b.installArtifact(zig);
 }
 
 const stubs = &.{
